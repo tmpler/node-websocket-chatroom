7d15f578b398c7e004d03bb0c920d06181350932
tmpler <mean@jsdev.uk>
2017-05-18 11:43:25 +0100

    chatui helpers

---
+++ public/js/chat_ui.js
@@ -0,0 +1,7 @@
+var divEscapedContentElement(message){
+  return $('<div></div>').text(message);
+}
+
+var divSystemContentElement(message){
+  return $('<div</div>').html('<i>' + message + '</i>');
+}
