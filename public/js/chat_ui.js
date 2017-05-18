b14a80d34c7f9bac63571958fcbac6a47b789ce8
tmpler <mean@jsdev.uk>
2017-05-18 11:50:57 +0100

    process raw user input

--- public/js/chat_ui.js
+++ public/js/chat_ui.js
@@ -1,7 +1,24 @@
-var divEscapedContentElement(message){
+var divEscapedContentElement = function(message){
   return $('<div></div>').text(message);
 }
 
-var divSystemContentElement(message){
+var divSystemContentElement = function(message){
   return $('<div</div>').html('<i>' + message + '</i>');
+}
+
+var processUserInput = function(chatApp, socket){
+  var message = $('#send-message').val();
+  var systemMessage;
+
+  if(message.charAt(0) == '/'){
+    systemMessage = chatApp.processCommand(message);
+    if(systemMessage){
+      $('#messages').append(divSystemContentElement(systemMessage));
+    }
+  } else {
+    chatApp.sendMessage($('#room').text(), message);
+    $('#messages').append(divEscapedContentElement(message));
+    $('#messages').scrollTop($('#messages').prop('scrollHeight'));
+  }
+  $('#send-message').val('');
 }
